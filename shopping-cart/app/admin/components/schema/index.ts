8463dcf49bd5b7eea5a885389ec67442dff8abdb
ahmad <kevetsahedrakh@hotmail.com>
2025-01-29 21:58:27 +0330

    feat:addSubCtegory form

--- shopping-cart/app/admin/components/schema/index.ts
+++ shopping-cart/app/admin/components/schema/index.ts
@@ -7,4 +7,10 @@ export const addCategorySchema = z.object({
   }),
 });
 
+export const addSubCategorySchema = z.object({
+  name: z.string().min(1, "Please add subCategory name"),
+  category: z.string().min(1, "Please add category name"),
+})
+
 export type AddCategorySchema = z.infer<typeof addCategorySchema>;
+export type AddSubCategorySchema = z.infer<typeof addSubCategorySchema>
