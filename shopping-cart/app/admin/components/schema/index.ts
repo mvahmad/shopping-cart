084e856d09e889866babdaf68d26d4661a990ae0
ahmad <kevetsahedrakh@hotmail.com>
2025-02-02 22:14:14 +0330

    feat : addProduct schema

--- shopping-cart/app/admin/components/schema/index.ts
+++ shopping-cart/app/admin/components/schema/index.ts
@@ -12,5 +12,25 @@ export const addSubCategorySchema = z.object({
   category: z.string().min(1, "Please add category name"),
 })
 
+export const schema = z.object({
+  name: z.string().min(1, "نام کتاب را وارد کنید."),
+  category:  z.string().min(1, "Please add category name"),
+  subcategory: z.string().min(1, "Please add subCategory name"),
+  brand: z.string().min(1, "Please add brand name"),
+  quantity: z.number({ message: " Please add qunantitiy" })
+    .min(0, "Please add qunantitiy"),
+  price: z
+    .number({ message: "Please add price" })
+    .min(0, "Please add price"),
+  discount: z
+    .number({ message: "Please add discount" })
+    .min(0, "Please add discount"),
+  thumbnail: z.any(),
+  images: z.any(),
+  description: z.string().min(1, "Please add discription"),
+});
+
+export type AddProduct = z.infer<typeof schema>;
+
 export type AddCategorySchema = z.infer<typeof addCategorySchema>;
 export type AddSubCategorySchema = z.infer<typeof addSubCategorySchema>
