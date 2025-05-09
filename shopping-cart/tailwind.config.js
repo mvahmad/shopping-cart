44633247677ebd96146303e42f3ea774d12201a7
ahmad <kevetsahedrakh@hotmail.com>
2025-05-09 10:27:35 +0330

    feat: update dependencies for Tailwind CSS and PostCSS integration

--- shopping-cart/tailwind.config.js
+++ shopping-cart/tailwind.config.js
@@ -1,10 +1,12 @@
-// /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 
 const {nextui} = require("@nextui-org/react");
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
  ],
