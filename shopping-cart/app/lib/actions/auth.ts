268b1eb160512e72ef1bb935eab8d680fd31a1d6
mvahmad <kevetsahedrakh@hotmail.com>
2025-07-31 16:31:05 +0330

    feat : postLoginData , postRefreshToken functions

--- shopping-cart/app/lib/actions/auth.ts
+++ shopping-cart/app/lib/actions/auth.ts
@@ -1,12 +1,36 @@
-"use server"
-//server actions
+import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
+import { ENDPOINTS } from "../../constant";
+import { httpRequest } from "../../services/http-request";
+import Cookies from "js-cookie";
 
-import { signIn , signOut} from "@/auth";
+export const {auth ,handlers , signIn ,signOut} = NextAuth({
+    providers:[GitHub]
+})
 
-export const loginGithub = async() => {
-   await signIn("github" ,{redirectTo:"/"})
-}
+type Token = { refreshToken: string };
 
-export const logoutGithub = async() => {
-   await signOut({redirectTo:"/"})
+export const postRegisterData = async (data: unknown) => {
+  const url = ENDPOINTS.AUTH.SIGNUP;
+  const response = await httpRequest.post(url, data);
+  return response.data;
+};
+
+export const postLoginData = async (data: unknown) => {
+  const url = ENDPOINTS.AUTH.LOGIN;
+  const response = await httpRequest.post(url, data);
+  return response.data;
+};
+
+export const postRefreshToken = async (token: Token) => {
+  const url = ENDPOINTS.AUTH.TOKEN;
+  const response = await httpRequest.post(url, token);
+  return response.data;
+};
+
+export const logout = async ({href}:{href:string})=>{
+  Cookies.remove("accessToken");
+  Cookies.remove("refreshToken");
+  localStorage.clear();
+  location.href = href;
 }
