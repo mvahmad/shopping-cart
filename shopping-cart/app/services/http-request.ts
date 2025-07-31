15a05436b8c7ed781fbe3c46af9d5ab984f4c8ca
mvahmad <kevetsahedrakh@hotmail.com>
2025-07-31 16:29:14 +0330

    feat: token for auth

--- shopping-cart/app/services/http-request.ts
+++ shopping-cart/app/services/http-request.ts
@@ -1,5 +1,42 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 import { BASE_URL } from "../constant";
+import { postRefreshToken , logout } from "@/app/lib/actions/auth";
+
 export const httpRequest = axios.create({
     baseURL:BASE_URL
 }) 
+
+httpRequest.interceptors.request.use(
+  (config) => {
+    const accessToken = Cookies.get("accessToken");
+    if (accessToken) config.headers.Authorization = `Bearer ${accessToken}`;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+httpRequest.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    const status = error.response.status;
+    const originRequest = error.config;
+    const refreshToken = Cookies.get("refreshToken");
+    if (status === 401 && refreshToken) {
+      return postRefreshToken({ refreshToken })
+        .then((res) => {
+          Cookies.set("accessToken", res.token.accessToken);
+          return httpRequest.request(originRequest);
+        })
+        .catch((error) => {
+          logout({ href: "/login" });
+          return Promise.reject(error);
+        });
+    }
+  }
+);
+
