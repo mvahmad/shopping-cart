3c084563c6c9dc5a9524d71695cc0fa6382a8f8c
mvahmad <kevetsahedrakh@hotmail.com>
2025-10-27 21:56:12 +0330

    fix : biuld error

--- app/admin/components/signOutButtn.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-import { logoutGithub } from "@/app/lib/actions/auth";
-import { Button } from "@nextui-org/react";
-export default function SignOutButton (){
-    return(
-        <Button onPress={()=>logoutGithub()}>sign Out</Button>
-    )
-}
