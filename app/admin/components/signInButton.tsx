bbcf650cc66adaea34986b2430fc9d69663245fa
mvahmad <kevetsahedrakh@hotmail.com>
2025-10-27 21:53:10 +0330

    fix : remove signInButton

--- app/admin/components/signInButton.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-import { loginGithub } from "@/app/lib/actions/auth";
-import { Button } from "@nextui-org/react";
-export default function SignInButton (){
-    return(
-        <Button onPress={()=>loginGithub()}>sign in with github</Button>
-    )
-}
