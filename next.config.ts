34941510e5d3b7e8ed6e9e142ddc62110c22c222
mvahmad <kevetsahedrakh@hotmail.com>
2025-11-16 10:23:20 +0330

    fix : use client components

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  env:{
-    GITHUB_ID:process.env.AUTH_GITHUB_ID,
-    GITHUB_SECRET:process.env.AUTH_GITHUB_SECRET
-  },
+  // env:{
+  //   GITHUB_ID:process.env.AUTH_GITHUB_ID,
+  //   GITHUB_SECRET:process.env.AUTH_GITHUB_SECRET
+  // },
 
   // 
 };
