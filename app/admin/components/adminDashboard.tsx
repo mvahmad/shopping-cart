041a4788f03782abc459555272efc8eac0b22d8f
mvahmad <kevetsahedrakh@hotmail.com>
2025-11-29 15:47:29 +0330

    fix: clean up AdminDashboard component by removing unused imports and simplifying layout

--- app/admin/components/adminDashboard.tsx
+++ app/admin/components/adminDashboard.tsx
@@ -1,40 +1,25 @@
 "use client"
 import type { ReactNode } from "react"
-import { Button} from "@nextui-org/react";
+// import { Button} from "@nextui-org/react";
 import AdminNav from "../components/adminNav";
-import { useState } from "react";
-import { usePathname } from "next/navigation";
+
 
 export default function AdminDashboard (
   {children , username , role}:
   {children:ReactNode,username?: string , role?:string})
   {
-    const [isDrawerOpen, setIsDrawerOpen] = useState(true)
-    const handleCartIconClick = () => {
-      setIsDrawerOpen(!isDrawerOpen)
-      
-    }
-    const pathname = usePathname()
-    const pageName = pathname.split('/').pop()
-    
-    
+  
   return ( 
-  <div className=" bg-gray-100 flex flex-row items-start  w-full h-screen  cursor-default">
-      {username  && <AdminNav isOpen={isDrawerOpen} role={role} info={username} onCartIconClick={handleCartIconClick} />}
+  <div dir="rtl"
+    className="min-h-screen bg-slate-100 text-slate-800 flex flex-col lg:flex-row">
+      {username  && <AdminNav 
+      role={role}
+      info={username}
+      />}
    
-    <div className="container max-h-full p-2 flex flex-col gap-2">
-      <div className="flex flex-col p-2 gap-2">
-            <h2 className="text-2xl flex gap-3 items-centers text-gray-600 font-semibold py-6">
-              {`${pageName} management`}
-            </h2>
-         <Button className="w-[3rem] mr-6" onPress={handleCartIconClick}>
-            Dashboard
-        </Button>
-      </div>
-      
+    <main className="flex-1 px-3 sm:px-6 lg:px-8 py-4 sm:py-6 space-y-4">
         {children}
-
-    </div>
+    </main>
    
   </div> );
 }
