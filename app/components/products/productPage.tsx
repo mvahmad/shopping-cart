15815841e3b54d41f49ffde635085060926db14d
mvahmad <kevetsahedrakh@hotmail.com>
2025-12-08 19:40:38 +0330

    feat:metadata for product page

---
+++ app/components/products/productPage.tsx
@@ -0,0 +1,96 @@
+"use client";
+import { getProductsResponse, ProductsEntity } from "@/app/types";
+import { MainBreadcrumb , Footer ,Header , useGetServices ,getProductById 
+  ,ProductInfo,SizeTable , ProductSlider
+} from "@/app/product/import" 
+import { GetProductsByIdResponse } from "@/app/product/type";
+import { useParams } from "next/navigation";
+import { A11y, Autoplay, Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination"
+import { getProducts } from "@/app/hooks/queryHooks/products";
+
+const ProductPage = () => {
+  //get product  by id 
+  const {id} = useParams()
+  const { data } = useGetServices<GetProductsByIdResponse>({
+      queryKey: ["GetBookById", id],
+      queryFn: () => getProductById(id! as string),
+      options: {
+        enabled: !!id,
+      },
+    });
+      
+    let endPrice = 0;
+    let totalPrice = 0;
+    let discountPercent = 0;
+    let product!: ProductsEntity;
+    if (data?.data.product) {
+      product = data.data.product;
+      const discount = data?.data.product.discount;
+      endPrice = data?.data?.product?.price;
+      if (discount !== 0) {
+        totalPrice = endPrice + discount;
+        discountPercent = Math.ceil((discount * 100) / endPrice);
+      }
+    }
+      //get product by same category
+      const { data: firstCategoryData, isLoading } =
+            useGetServices<getProductsResponse>({
+            queryKey: ["GetFirstCategoryBooks", product],
+            queryFn: () => getProducts({ limit: "6", category: product?.category._id }),
+        });
+ 
+    const images = product?.images;
+    const firstCategoryItems = firstCategoryData?.data?.products || [];
+
+
+    return ( <>
+    <Header />
+    <section className="[Product Page] py-3 px-5 flex  flex-col  ">
+        <MainBreadcrumb product={product} type="single" />
+        <div className=" flex md:flex-row flex-col justify-evenly md:items-start items-center gap-1">
+
+          <div className="sm:w-[380px] w-[220px] ">
+            <Swiper
+              modules={[Navigation, Pagination, A11y, Autoplay]}
+              slidesPerView={1}
+              pagination={{ clickable: true }}
+              scrollbar={{ draggable: true }}
+              navigation
+            >
+              {images?.map((image) => (
+                <SwiperSlide key={image}>
+                  <div className="flex justify-center items-center">
+                    <img
+                      src={image}
+                      alt={name?.[0]}
+                      className="sm:w-[400px] w-[200px] rounded-lg max-h-[400px] object-contain"
+                    />
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+          <ProductInfo product={product} />
+        </div>
+
+           <SizeTable />
+           <ProductSlider
+              products={firstCategoryItems}
+              isLoading={isLoading}
+              title={'محصولات مرتبط'}
+              bg={'bg-white'}
+              text={"text-slate-700"}
+           />
+
+           
+    </section>
+      
+    <Footer />
+    </> );
+}
+ 
+export default ProductPage;
--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,96 +1,54 @@
-"use client";
-import { getProductsResponse, ProductsEntity } from "@/app/types";
-import { MainBreadcrumb , Footer ,Header , useGetServices ,getProductById 
-  ,ProductInfo,SizeTable , ProductSlider
-} from "@/app/product/import" 
-import { GetProductsByIdResponse } from "../type";
-import { useParams } from "next/navigation";
-import { A11y, Autoplay, Navigation, Pagination } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination"
-import { getProducts } from "@/app/hooks/queryHooks/products";
+import ProductPage from "@/app/components/products/productPage";
+import { Metadata } from "next";
+import { getProductById } from "../import";
 
-const ProductPage = () => {
-  //get product  by id 
-  const {id} = useParams()
-  const { data } = useGetServices<GetProductsByIdResponse>({
-      queryKey: ["GetBookById", id],
-      queryFn: () => getProductById(id! as string),
-      options: {
-        enabled: !!id,
-      },
-    });
-      
-    let endPrice = 0;
-    let totalPrice = 0;
-    let discountPercent = 0;
-    let product!: ProductsEntity;
-    if (data?.data.product) {
-      product = data.data.product;
-      const discount = data?.data.product.discount;
-      endPrice = data?.data?.product?.price;
-      if (discount !== 0) {
-        totalPrice = endPrice + discount;
-        discountPercent = Math.ceil((discount * 100) / endPrice);
-      }
+type MetadataProps = {
+  params: {
+    id: string;
+  };
+};
+
+export async function generateMetadata({ params }: MetadataProps): Promise<Metadata> {
+  const id = params.id;
+
+  try {
+    const product = await getProductById(id);
+    console.log("PRODUCT METADATA:", product);
+    
+
+    if (!product) {
+      return {
+        title: "محصول یافت نشد",
+        description: "این محصول موجود نیست.",
+      };
     }
-      //get product by same category
-      const { data: firstCategoryData, isLoading } =
-            useGetServices<getProductsResponse>({
-            queryKey: ["GetFirstCategoryBooks", product],
-            queryFn: () => getProducts({ limit: "6", category: product?.category._id }),
-        });
- 
-    const images = product?.images;
-    const firstCategoryItems = firstCategoryData?.data?.products || [];
 
-
-    return ( <>
-    <Header />
-    <section className="[Product Page] py-3 px-5 flex  flex-col  ">
-        <MainBreadcrumb product={product} type="single" />
-        <div className=" flex md:flex-row flex-col justify-evenly md:items-start items-center gap-1">
-
-          <div className="sm:w-[380px] w-[220px] ">
-            <Swiper
-              modules={[Navigation, Pagination, A11y, Autoplay]}
-              slidesPerView={1}
-              pagination={{ clickable: true }}
-              scrollbar={{ draggable: true }}
-              navigation
-            >
-              {images?.map((image) => (
-                <SwiperSlide key={image}>
-                  <div className="flex justify-center items-center">
-                    <img
-                      src={image}
-                      alt={name?.[0]}
-                      className="sm:w-[400px] w-[200px] rounded-lg max-h-[400px] object-contain"
-                    />
-                  </div>
-                </SwiperSlide>
-              ))}
-            </Swiper>
-          </div>
-          <ProductInfo product={product} />
-        </div>
-
-           <SizeTable />
-           <ProductSlider
-              products={firstCategoryItems}
-              isLoading={isLoading}
-              title={'محصولات مرتبط'}
-              bg={'bg-white'}
-              text={"text-slate-700"}
-           />
-
-           
-    </section>
-      
-    <Footer />
-    </> );
+    return {
+      title: `${product.data.product.name} | Elite Sport`,
+      description: product.description ?? "مشاهده جزئیات محصول",
+      openGraph: {
+        title: product.data.product.name,
+        description: product.data.product.description,
+        images: product.data.product.images?.length ? product.data.product.images : [],
+        type: "website",
+      },
+      twitter: {
+        card: "summary_large_image",
+        title: product.data.product.name,
+        description: product.data.product.description,
+        images: product.data.product.images?.[0],
+      },
+    };
+  } catch (err) {
+    return {
+      title: "خطای بارگذاری محصول",
+    };
+  }
 }
- 
-export default ProductPage;
+
+
+const Page = () => {
+  return <ProductPage />
+} 
+
+export default Page;
